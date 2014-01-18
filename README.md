855600e8b8881f4f07bfada9cb9ed534dbab22a7
Rupert Muchembled <rupert@rmuch.net>
2014-01-18 13:34:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# jsprotect: protect data from crawlers
+
+## Introduction
+
+jsprotect is a simple library to help protect semi-confidential data, such as
+addresses, e-mail addresses and telephone numbers from web crawlers and
+scrapers.
+
+jsprotect is not a foolproof guarantee that your e-mail address will not be
+scraped, but can mitigate against more naive web crawlers.
+
+## License
+
+See LICENSE.txt.
+
+## Disclaimer
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
---
+++ jsprotect.js
@@ -0,0 +1,53 @@
+﻿
+function jsprotect_strrev(str) {
+    return str.split('').reverse().join('');
+}
+
+function jsprotect_encode(str) {
+    return btoa(jsprotect_strrev(str));
+}
+
+function jsprotect_decode(str) {
+    return jsprotect_strrev(atob(str));
+}
+
+function jsprotect_apply(id) {
+    var tag = document.getElementById(id);
+
+    var dataAttr = tag.getAttribute('jsprotect-data');
+    if (dataAttr) {
+        tag.innerHTML = jsprotect_decode(dataAttr.valueOf());
+    }
+
+    var hrefAttr = tag.getAttribute('jsprotect-href');
+    if (hrefAttr) {
+        tag.setAttribute('href', jsprotect_decode(hrefAttr.valueOf()));
+    }
+}
+
+function jsprotect_run(ids) {
+    var startTime = new Date().getTime();
+
+    // Wait time (in ms) can be adjusted to a higher arbitrary value to
+    // mitigate against some automated screenshot tools.
+    var wait = 192 + Math.floor(Math.random() * 384);
+
+    setTimeout(function() {
+        var completionTime = new Date().getTime();
+        var delta = completionTime - startTime;
+
+        // Probably not particularly useful, but we can detect here if the
+        // JavaScript engine is running at a faster speed but still correctly
+        // reporting time.
+        var tolerance = 64;
+        var deltaWait = Math.abs(wait - delta);
+        if (deltaWait > tolerance) {
+            return;
+        }
+
+        var idArray = ids.split(' ');
+        idArray.forEach(function (a) {
+            jsprotect_apply(a);
+        });
+    }, wait);
+}
